97a393c6f0af1f3e24797778c0a3451c97be0c2f
Nikita <78035525+outref@users.noreply.github.com>
2021-02-02 05:04:12 +0200

    major update

---
+++ homejs.js
@@ -0,0 +1,55 @@
+var rowNum = 1; // row counter
+var rowAm; // items in one row
+var totalAm = 0; //total amount of items
+var largestBlockSize = 0;
+
+function addItem() {
+	var div = document.createElement("div"); //addind rows for items
+	div.setAttribute("id","list" + rowNum);
+	div.setAttribute("class","input-group");
+	div.setAttribute("style","margin-top:3vh");
+	div.innerHTML = "<input id = \"am" + rowNum + "\" type=\"text\" aria-label=\"Amount\" class=\"form-control\" style=\"width:10%; padding:2px 2px\" value=\"1\">\
+  					<input id = \"size" + rowNum + "\" type=\"text\" aria-label=\"Size\" class=\"form-control\" style=\"width:15%\" placeholder=\"Size\">\
+  					<input id= \"speed" + rowNum + "\" type=\"text\" aria-label=\"Speed\" class=\"form-control\" style=\"width:20%\" placeholder=\"Speed\">\
+  					<input id= \"emo" + rowNum + "\" type=\"text\" aria-label=\"Emo\" class=\"form-control\" style=\"width:20%\" placeholder=\"Emodji\">\
+  					<select class=\"form-select\" id=\"behavior" + rowNum + "\" style=\"width:35%\">\
+    					<option selected value=\"random\">Random</option>\
+    					<option value=\"crazy\">Crazy</option>\
+    					<option value=\"physicalCons\">Physical(constant speed)</option>\
+    					<option value=\"physicalFade\">Physical(fading speed)</option>\
+    					<option value=\"player\">Player</option>\
+  					</select>";
+	document.getElementById("list").appendChild(div);
+	rowNum++;
+};
+
+function begin () {
+	localStorage.clear();
+
+	if (document.getElementById("fullscreen").checked) { //saving room sizes
+	localStorage.setItem("fullscreen", "on");
+	}	
+	localStorage.setItem("width", document.getElementById("width").value);
+	localStorage.setItem("height", document.getElementById("height").value);
+
+	localStorage.setItem("colorInput", document.getElementById("colorInput").value); //saving color
+
+	for (var i=0;i<rowNum;i++) { //saving all items
+		rowAm = document.getElementById("am"+i).value; //amount of items in a row
+		for (var j=0;j<rowAm;j++) {
+			localStorage.setItem("size"+totalAm, document.getElementById("size"+i).value);
+			localStorage.setItem("speed"+totalAm, document.getElementById("speed"+i).value);
+			localStorage.setItem("emo"+totalAm, document.getElementById("emo"+i).value.replace(/\s/g, '') );
+			localStorage.setItem("behavior"+totalAm, document.getElementById("behavior"+i).value);
+			totalAm = totalAm + 1;
+			if (document.getElementById("size"+i).value>largestBlockSize) { //finding the largest block
+				largestBlockSize = document.getElementById("size"+i).value;
+			};
+		};
+	};
+
+	localStorage.setItem("amountOfBlocks",totalAm);
+	localStorage.setItem("largestBlockSize",largestBlockSize);
+
+	location.href ="game.html";
+};
--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+
 var room = document.getElementById("room");
-var roomWidth = prompt("Print room width! (pixels)");
+var roomWidth;
 var roomHeight;
 
-if (roomWidth === "fullscreen") {             //adding fullscreen option
-	room.style.width = screen.width + "px";
-	room.style.height = screen.height + "px";
+if (localStorage.getItem("fullscreen") === "on") {             //adding fullscreen option
+	roomWidth = screen.width - Number(localStorage.getItem("largestBlockSize"))*0.36; // substracting border size to accomodate room with borders
+	roomHeight = screen.height*0.802 - Number(localStorage.getItem("largestBlockSize"))*0.36;
 
-	roomWidth = screen.width;
-	roomHeight = screen.height;
+	room.style.width = roomWidth + "px";
+	room.style.height = roomHeight + "px";
 } else {
-	roomHeight = prompt("Print room height! (pixels)");
+	roomWidth = localStorage.getItem("width");
+	roomHeight = localStorage.getItem("height");
 	room.style.height = roomHeight + "px";
 	room.style.width = roomWidth + "px";
 	room.style.border = "6px solid black";
 }
+room.style.backgroundColor = localStorage.getItem("colorInput");
+room.style.border = Number(localStorage.getItem("largestBlockSize"))*0.18 + "px double white"; //making borders for border collisions
 
+var amountOfBlocks = localStorage.getItem("amountOfBlocks");
 
-var amountOfBlocks = prompt("How many crazy blocks do you want?");
-
-var blocks = []; //declaration of array of objects
+var blocks = []; //declaration of array of objects(blocks)
 
 var availPosLeft = 0; // available positions for addElement()
 var availPosTop = 0;
@@ -35,7 +38,7 @@ function position (i) {
 }
 
 function randomizer (i) { 
-	blocks[i].randomDir = Math.floor(Math.random() * 4) + 1; //random number for direction
+	blocks[i].randomDir = Math.floor(Math.random() * 8) + 1; //random number for direction
 	blocks[i].randomSteps = Math.floor(Math.random() * (blocks[i].blockStepsMax-blocks[i].blockStepsMin)) + blocks[i].blockStepsMin; //random number for amount of steps in one direction (between min and max)
 	blocks[i].j = 1; // counter for amount of steps in one direction
 }
@@ -50,7 +53,7 @@ function addElement (i) { //creates block in HTML
 	div.style.margin = "0px 0px 0px 0px";
 	div.style.padding = "0px 0px 0px 0px";
 	div.style.position = "absolute";
-	div.innerHTML = "😺";
+	div.innerHTML = localStorage.getItem("emo"+i);
 
 	// div.style.background = "blue";  //for collision testing
 	// if (i===1) {div.style.background = "red"};
@@ -58,10 +61,10 @@ function addElement (i) { //creates block in HTML
 	if (availPosLeft + blocks[i].blockSize > roomWidth) { //blocks positioning inside the room without overlaping 
 		if (availPosTopNext + blocks[i].blockSize > roomHeight) { //problems can occur in non-rectangular rooms
 			alert("Too many or too large items in the room!");
-			location.reload();
+			location.href ="index.html";
 		};
 
-		div.style.top = availPosTopNext + 1 + "px";
+		div.style.top = availPosTopNext + blocks[i].blockStep + 1 + "px"; //new row
 		div.style.left = "0px";
 
 		availPosLeft = blocks[i].blockSize;
@@ -69,11 +72,11 @@ function addElement (i) { //creates block in HTML
 		availPosTopNext = availPosTop + blocks[i].blockSize;
 
 	} else {
-		div.style.top = availPosTop + 1 + "px";
-		div.style.left = availPosLeft + 1 + "px";
+		div.style.top = availPosTop + blocks[i].blockStep + 1 + "px"; //old row
+		div.style.left = availPosLeft + blocks[i].blockStep + 1 + "px";
 
 		availPosLeft = availPosLeft + blocks[i].blockSize;
-		if (availPosTop + blocks[i].blockSize > availPosTopNext) {
+		if (availPosTop + blocks[i].blockSize > availPosTopNext) { //finding highest block for the next row
 			availPosTopNext = availPosTop + blocks[i].blockSize;
 		};
 	};
@@ -87,10 +90,18 @@ function createBlocks() {
 
 		blocks[i] = {}; // declaration of object insde of the array
 
-		blocks[i].blockSize = Number(prompt("Print block N" + (i+1) + " size! (pixels)")); // size of the object
-		blocks[i].blockStep = Number(prompt("Print block N" + (i+1) + " speed!")); // length oh single object's step: changes it's individual speed
-		blocks[i].blockStepsMin = ((Number(roomWidth)+Number(roomHeight))/2)/blocks[i].blockStep/4; // minimal part of the room in one direction -- average of room's width and height used, important for non-rectangular rooms
-		blocks[i].blockStepsMax = ((Number(roomWidth)+Number(roomHeight))/2)/blocks[i].blockStep/2; // maximal part of the room in one direction
+		blocks[i].blockSize = Number(localStorage.getItem("size"+i)); // size of the object
+		blocks[i].blockStep = Number(localStorage.getItem("speed"+i)); // length oh single object's step: changes it's individual speed
+		blocks[i].behavior = localStorage.getItem("behavior"+i);
+
+		var min; var max; //minimum and maximum steps divider
+		if (blocks[i].behavior==="random") {min=6;max=3;} else if (blocks[i].behavior==="crazy") {min=10;max=5;} else {min=1;max=1;};
+		blocks[i].blockStepsMin = ((Number(roomWidth)+Number(roomHeight))/2)/blocks[i].blockStep/min; // minimal part of the room in one direction -- average of room's width and height used, important for non-rectangular rooms
+		blocks[i].blockStepsMax = ((Number(roomWidth)+Number(roomHeight))/2)/blocks[i].blockStep/max; // maximal part of the room in one direction
+
+		if (blocks[i].behavior==="physicalCons" || blocks[i].behavior==="physicalFade") { //not increasing j for physical objects(no random turns)
+			blocks[i].jBehavior = "off";
+		} else {blocks[i].jBehavior = "on";};
 
 		randomizer (i); //randomizes numbers for the first move
 
@@ -110,14 +121,14 @@ function allBlocksMovement(){
 
 		for (var z=0; z<amountOfBlocks; z++) {  // collision detection with all blocks
 			if (z !== i) { 						// aviodind detecting collision with itself
-				if (blocks[i].x - blocks[i].blockSize/3 < blocks[z].x + blocks[z].blockSize/3 && //left collision
-   				blocks[i].x + blocks[i].blockSize/3 > blocks[z].x - blocks[z].blockSize/3 && // right collision
-  				blocks[i].y - blocks[i].blockSize/3 < blocks[z].y + blocks[z].blockSize/3 && // top collision
-   				blocks[i].y + blocks[i].blockSize/3 > blocks[z].y - blocks[z].blockSize/3) { // bottom collision
+				if (blocks[i].x - (blocks[i].blockSize/2)*0.66 < blocks[z].x + (blocks[z].blockSize/2)*0.66 && //0.66 is used to ajust emojis collision model
+   				blocks[i].x + (blocks[i].blockSize/2)*0.66 > blocks[z].x - (blocks[z].blockSize/2)*0.66 && 
+  				blocks[i].y - (blocks[i].blockSize/2)*0.66 < blocks[z].y + (blocks[z].blockSize/2)*0.66 && 
+   				blocks[i].y + (blocks[i].blockSize/2)*0.66 > blocks[z].y - (blocks[z].blockSize/2)*0.66) { 
 
-					blocks[z].randomDir = blocks[i].randomDir; //bumping victim block
+					blocks[z].randomDir = blocks[i].randomDir; //bumping victim block to the current block movement direction
 
-					if (blocks[i].randomDir === 1 || blocks[i].randomDir === 3) { //reversing current block movement direction
+					if (blocks[i].randomDir === 1 || blocks[i].randomDir === 3 || blocks[i].randomDir === 5 || blocks[i].randomDir === 7) { //reversing current block movement direction
 						blocks[i].randomDir = blocks[i].randomDir + 1;
 					} else {
 						blocks[i].randomDir = blocks[i].randomDir - 1;
@@ -130,6 +141,8 @@ function allBlocksMovement(){
 					blocks[z].randomSteps = Math.floor(Math.random() * (blocks[i].blockStepsMax-blocks[i].blockStepsMin)) + blocks[i].blockStepsMin; 
 					blocks[z].j = 1;
 
+					if (blocks[z].behavior==="physicalFade") {blocks[z].blockStep=blocks[i].blockStep}; //accelerating fading speed object back
+
 					move(); // move twice to get out of the collision situation
 					move();
 				};
@@ -139,63 +152,166 @@ function allBlocksMovement(){
 
 	 	function move () {
 
+	 		if (blocks[i].jBehavior === "off") {blocks[i].j=0}; //stopping j count for physical objects
+
+	 		if (blocks[i].behavior==="physicalFade") {blocks[i].blockStep=blocks[i].blockStep*0.997}; //fading speed
+
+	 		//if (blocks[i].behavior==="player")
+
 			position(i);
 
 			switch (blocks[i].randomDir) {
 
-			case 1 :
-				if (blocks[i].j>blocks[i].randomSteps || blocks[i].x + (blocks[i].blockSize/2) + blocks[i].blockStep > Number(roomWidth) ) {
+			case 1 : //right
+				if (blocks[i].j>blocks[i].randomSteps) { //end of one direction move
 					randomizer (i);
+				} else if (blocks[i].x + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomWidth)) {
+					randomizer (i);
+					blocks[i].randomDir = 2;
+					moveLeft(); // wall bump
 				} else {moveRight()};
 			break;
 
-			case 2 : 
-				if (blocks[i].j>blocks[i].randomSteps || blocks[i].x - (blocks[i].blockSize/2) - blocks[i].blockStep < 0 ) {
+			case 2 : //left
+				if (blocks[i].j>blocks[i].randomSteps) {
 					randomizer (i);
+				} else if (blocks[i].x - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0) {
+					randomizer (i);
+					blocks[i].randomDir = 1;
+					moveRight(); 
 				} else {moveLeft()};
 			break;
 
-			case 3 :
-				if (blocks[i].j>blocks[i].randomSteps || blocks[i].y + (blocks[i].blockSize/2) + blocks[i].blockStep > Number(roomHeight) ) {
+			case 3 : //down
+				if (blocks[i].j>blocks[i].randomSteps) {
 					randomizer (i);
+				} else if (blocks[i].y + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomHeight)) {
+					randomizer (i);
+					blocks[i].randomDir = 4;
+					moveUp();
 				} else {moveDown()};
 			break;
 
-			case 4 : 	
-				if (blocks[i].j>blocks[i].randomSteps || blocks[i].y - (blocks[i].blockSize/2) - blocks[i].blockStep < 0 ) {
+			case 4 : //up
+				if (blocks[i].j>blocks[i].randomSteps) {
 					randomizer (i);
+				} else if (blocks[i].y - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0 ) {
+					randomizer (i);
+					blocks[i].randomDir = 3;
+					moveDown();
 				} else {moveUp()};
+			break;
+
+			case 5 : //right-down
+				if (blocks[i].j>blocks[i].randomSteps) { //end of one direction move
+					randomizer (i);
+				} else if (blocks[i].x + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomWidth)) { // right wall bump
+					randomizer (i);
+					blocks[i].randomDir = 7;
+					moveLeftDown();
+				} else if (blocks[i].y + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomHeight)) {//bottom wall bump
+					randomizer (i);
+					blocks[i].randomDir = 8;
+					moveRightUp();
+				} else {moveRightDown()};
+			break;
+
+			case 6 : //left - up
+				if (blocks[i].j>blocks[i].randomSteps) {
+					randomizer (i);
+				} else if (blocks[i].x - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0) { //left wall bump
+					randomizer (i);
+					blocks[i].randomDir = 8;
+					moveRightUp(); 
+				} else if (blocks[i].y - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0 ) {//top wall bump
+					randomizer (i);
+					blocks[i].randomDir = 7;
+					moveLeftDown();
+				} else {moveLeftUp()};
+			break;
+
+			case 7 : //left-down
+				if (blocks[i].j>blocks[i].randomSteps) {
+					randomizer (i);
+				} else if (blocks[i].y + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomHeight)) { //bottom wall bump
+					randomizer (i);
+					blocks[i].randomDir = 6;
+					moveLeftUp();
+				} else if (blocks[i].x - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0 ) {//left wall bump
+					randomizer (i);
+					blocks[i].randomDir = 5;
+					moveRightDown();
+				} else {moveLeftDown()};
+			break;
+
+			case 8 : //right-up	
+				if (blocks[i].j>blocks[i].randomSteps) {
+					randomizer (i);
+				} else if (blocks[i].y - (blocks[i].blockSize/2)*0.62 - blocks[i].blockStep < 0 ) { //top wall bump
+					randomizer (i);
+					blocks[i].randomDir = 5;
+					moveRightDown();
+				} else if (blocks[i].x + (blocks[i].blockSize/2)*0.62 + blocks[i].blockStep > Number(roomWidth)) { // right wall bump
+					randomizer (i);
+					blocks[i].randomDir = 6;
+					moveLeftUp();
+				} else {moveRightUp()};
 			break;
 
 			default :
 				alert("Something bad happned ;(");
-			}
+			};
 
 
 			function moveRight () {
 				block.style.left = (Number(block.style.left.slice(0, -2)) + blocks[i].blockStep) + "px";
+				block.style.transform = "scale(-1, 1)"; //flippin
 				blocks[i].j =blocks[i].j + 1;
-			}
+			};
 
 			function moveDown () {
 				block.style.top = (Number(block.style.top.slice(0, -2)) + blocks[i].blockStep) + "px";
 				blocks[i].j =blocks[i].j + 1;
-			}
+			};
 
 			function moveLeft () {
 				block.style.left = (Number(block.style.left.slice(0, -2)) - blocks[i].blockStep) + "px";
+				block.style.transform = "scale(1, 1)";
 				blocks[i].j =blocks[i].j + 1;
-			}
+			};
 
 			function moveUp () {
 				block.style.top = (Number(block.style.top.slice(0, -2)) - blocks[i].blockStep) + "px";
 				blocks[i].j =blocks[i].j + 1;
-			}
-		}	
-	}
-}
+			};
 
+			function moveRightDown () {
+				block.style.left = (Number(block.style.left.slice(0, -2)) + blocks[i].blockStep/2) + "px"; //right
+				block.style.top = (Number(block.style.top.slice(0, -2)) + blocks[i].blockStep/2) + "px"; //down
+				block.style.transform = "scale(-1, 1)";
+				blocks[i].j =blocks[i].j + 1;
+			};
 
+			function moveLeftDown () {
+				block.style.left = (Number(block.style.left.slice(0, -2)) - blocks[i].blockStep/2) + "px"; //left
+				block.style.top = (Number(block.style.top.slice(0, -2)) + blocks[i].blockStep/2) + "px"; //down
+				block.style.transform = "scale(1, 1)";
+				blocks[i].j =blocks[i].j + 1;
+			};
 
+			function moveLeftUp () {
+				block.style.left = (Number(block.style.left.slice(0, -2)) - blocks[i].blockStep/2) + "px"; //left
+				block.style.top = (Number(block.style.top.slice(0, -2)) - blocks[i].blockStep/2) + "px"; //up
+				block.style.transform = "scale(1, 1)";
+				blocks[i].j =blocks[i].j + 1;
+			};
 
-
+			function moveRightUp () {
+				block.style.left = (Number(block.style.left.slice(0, -2)) + blocks[i].blockStep/2) + "px"; //right
+				block.style.top = (Number(block.style.top.slice(0, -2)) - blocks[i].blockStep/2) + "px"; //up
+				block.style.transform = "scale(-1, 1)";
+				blocks[i].j =blocks[i].j + 1;
+			};
+		};
+	};
+};
